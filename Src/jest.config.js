3bdeb662d44daea6e7bfa12dbd529e18ffa552a6
ignatandrei <ignatandrei@yahoo.com>
2019-07-28 11:14:48 +0300

    dockerize and run tests

--- Src/jest.config.js
+++ Src/jest.config.js
@@ -4,5 +4,5 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testEnvironment: 'node',
-  collectCoverage: false,  
+  collectCoverage: true,  
 };
--- dockerize/ci_test.bat
+++ dockerize/ci_test.bat
@@ -1,1 +1,4 @@
 docker build ../src -f docker_ci_test.txt -t bingo_ci_test
+docker run -d --rm --name bingo_ci_test_container bingo_ci_test
+docker cp bingo_ci_test_container:/app/jest-stare .	
+docker container kill bingo_ci_test_container

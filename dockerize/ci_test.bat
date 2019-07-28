8af98f17d4a1604219714562142af79142b77209
ignatandrei <ignatandrei@yahoo.com>
2019-07-28 19:20:12 +0300

    pipelines

--- dockerize/ci_test.bat
+++ dockerize/ci_test.bat
@@ -1,4 +1,5 @@
 docker build ../Src -f docker_ci_test.txt -t bingo_ci_test
 docker run -d --rm --name bingo_ci_test_container bingo_ci_test
-docker cp bingo_ci_test_container:/app/jest-stare .	
+docker cp bingo_ci_test_container:/app/jest-stare .
+docker cp bingo_ci_test_container:/app/junit.xml .	
 docker container kill bingo_ci_test_container

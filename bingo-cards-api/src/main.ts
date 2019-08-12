11dde732a160e38b98b8766713c497f9c4a7ea06
ignatandrei <ignatandrei@yahoo.com>
2019-08-12 23:41:06 +0300

    preparing for heroku

--- bingo-cards-api/src/main.ts
+++ bingo-cards-api/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();

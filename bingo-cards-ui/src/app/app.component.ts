07dd147653225a8f438b372a24651b0d22626f46
ignatandrei <ignatandrei@yahoo.com>
2019-08-17 19:09:48 +0300

    first iteration of cards

--- bingo-cards-ui/src/app/app.component.ts
+++ bingo-cards-ui/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {Meeting} from 'bingo-meeting-objects';
+import {CreateMeeting} from 'bingo-cards-api-objects';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'bingo-cards-ui';
+  meetings: Meeting[];
+  createMeetingForm: FormGroup;
+
+  constructor( private formBuilder: FormBuilder) {
+    this.meetings = [];
+    // why this gives an compilation error? ???
+    // const c: CreateMeeting = new CreateMeeting();
+    this.createMeetingForm = this.formBuilder.group({userName: '', meetingName: ''});
+  }
+  onSubmit(data: any) {
+    // Process checkout data here
+    window.alert(JSON.stringify(data));
+    this.createMeetingForm.reset();
+  }
 }

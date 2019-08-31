4bd3c1c513fb8550a933aaca4350f5c9406dcca1
ignatandrei <ignatandrei@yahoo.com>
2019-08-31 07:50:01 +0300

    adding source docs

--- dockerize/build_code_documentation.bat
+++ dockerize/build_code_documentation.bat
@@ -1,4 +1,4 @@
-docker build .. -f docker_build_web.txt -t bingo_build_web
-docker run -d --rm --name bingo_build_web_container bingo_build_web
-docker cp bingo_build_web_container:/app/bingo-cards-ui/dist .
-docker container kill bingo_build_web_container
+docker build .. -f docker_build_code_documentation.txt -t bingo_build_code_documentation
+docker run -d --rm --name bingo_build_code_documentation_container bingo_build_code_documentation
+docker cp bingo_build_code_documentation_container:/app/docs/source .
+docker container kill bingo_build_code_documentation_container
--- dockerize/build_docs.bat
+++ dockerize/build_docs.bat
@@ -1,3 +1,5 @@
-call build_web.bat
 del /S /Q ..\docs\*.*
+call build_web.bat
 copy dist\bingo-cards-ui\*.* ..\docs\
+call build_code_documentation.bat
+xcopy source\ ..\docs\source /S /I
